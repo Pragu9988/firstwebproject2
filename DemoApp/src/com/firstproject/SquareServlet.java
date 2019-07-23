2c16e563f63d6550397bd1231102eeed3d3e6bab
Pragyan Chauadhary <chaudharypragyan@gmail.com>
2019-07-23 23:08:25 +0545

    commit all files

---
+++ DemoApp/src/com/firstproject/SquareServlet.java
@@ -0,0 +1,25 @@
+package com.firstproject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SquareServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		HttpSession session = req.getSession();
+
+		int k = (int) session.getAttribute("k");
+		
+		k = k*k;
+		PrintWriter out = res.getWriter();
+		out.println("Result is"+k);
+System.out.println("SQ Called");
+	}	
+
+}
